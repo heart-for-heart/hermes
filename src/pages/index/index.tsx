c1ec9a8bb8bb24c654975165bb33591358c57c97
wangya <wangya@kezaihui.com>
2023-10-26 22:09:03 +0800

    首页 & 增加10张松鼠gif

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,16 +1,31 @@
-import { View, Text } from '@tarojs/components'
-import { useLoad } from '@tarojs/taro'
-import './index.scss'
+import { View} from "@tarojs/components"
+import React, { useEffect, useState } from "react"
 
-export default function Index() {
+import "./index.scss"
 
-  useLoad(() => {
-    console.log('Page loaded.')
-  })
+const Index: React.FC = () => {
+  // TODO：需要加loading状态
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false)
+    }, 2000)
+  }, [])
 
   return (
-    <View className='index'>
-      <Text>Hello world!</Text>
-    </View>
+    // <HuiLoader loading={loading}>
+      <View className="index-page">
+        <View className="dialog-box">
+          <View className="first-line">您好，我是您的门店管家～</View>
+          <View className="second-line">您可以叫我小蜜</View>
+          <View className="third-line">
+            接下来由我代您开启一段餐饮门店经营之旅
+          </View>
+        </View>
+      </View>
+    // </HuiLoader>
   )
 }
+
+export default Index
