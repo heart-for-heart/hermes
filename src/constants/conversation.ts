f4a0c096017205edc460c7c5490010f33626a3ba
yanghaixiao <yanghaixiao@kezaihui.com>
2023-10-26 20:45:38 +0800

    添加松鼠gif

--- src/constants/conversation.ts
+++ src/constants/conversation.ts
@@ -90,3 +90,23 @@ const conversations = [
 ];
 
 export const formatConversations = conversations.map((c) => parseDialog(c));
+
+export const GifSoures = [{
+  name: '不想动',
+  src: '../../gifs/7628.GIF',
+},{
+  name: '再见',
+  src: '../../gifs/7629.GIF',
+},{
+  name: '膜拜',
+  src: '../../gifs/7630.GIF',
+},{
+  name: '摇头',
+  src: '../../gifs/7631.GIF',
+},{
+  name: '开心',
+  src: '../../gifs/7632.GIF',
+},{
+  name: '飞翔',
+  src: '../../gifs/7633.GIF',
+}]
