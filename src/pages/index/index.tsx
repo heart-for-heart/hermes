4ac35a2cd88638bc2a4ea8290556a2ffaf50ed8e
wangya <wangya@kezaihui.com>
2023-10-27 11:26:37 +0800

    首页打字机效果完成

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,51 +1,39 @@
-import { View ,  Button} from "@tarojs/components"
+import { View } from "@tarojs/components"
 import React, { useEffect, useState } from "react"
-import Taro from "@tarojs/taro"
+
+import TextTyper from "@/components/TextTyper"
 
 import "./index.scss"
 
 const Index: React.FC = () => {
-  // TODO：需要加loading状态
-  const [loading, setLoading] = useState(true)
+  const [showFirst, setShowFirst] = useState(true)
+  const [showSecond, setShowSecond] = useState(false)
+  const [showThird, setShowThird] = useState(false)
+
+  const text = [
+    "您好，我是您的门店管家～",
+    "您可以叫我小蜜",
+    "接下来由我代您开启一段餐饮门店经营之旅",
+  ]
 
   useEffect(() => {
     setTimeout(() => {
-      setLoading(false)
-    }, 2000)
+      setShowSecond(true)
+    }, 1500)
+
+    setTimeout(() => {
+      setShowThird(true)
+    }, 2500)
   }, [])
 
   return (
-    // <HuiLoader loading={loading}>
-      <View className="index-page">
-        <View className="dialog-box">
-          <View className="first-line">您好，我是您的门店管家～</View>
-          <View className="second-line">您可以叫我小蜜</View>
-          <View className="third-line">
-            接下来由我代您开启一段餐饮门店经营之旅
-          </View>
-        </View>
-        <View style={{ paddingTop: 100 }}>
-      <Button
-        onClick={() => {
-          Taro.navigateTo({
-            url: "/pages/ChatRoom/index?type=morning",
-          });
-        }}
-      >
-        早上场景
-      </Button>
-      <Button
-        onClick={() => {
-          Taro.navigateTo({
-            url: "/pages/ChatRoom/index?type=afternoon",
-          });
-        }}
-      >
-        下午场景
-      </Button>
+    <View className="index-page">
+      <View className="dialog-box">
+        {showFirst && text[0] && <TextTyper value={text[0]} />}
+        {showSecond && text[1] && <TextTyper value={text[1]} />}
+        {showThird && text[2] && <TextTyper value={text[2]} />}
       </View>
-      </View>
-    // </HuiLoader>
+    </View>
   )
 }
 
